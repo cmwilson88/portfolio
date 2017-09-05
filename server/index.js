c4f07c743ca68796df0bb00eb016101091cc04b2
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-05 13:36:30 -0600

    add index.js for server code

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express'),
+	  app = express();
+
+app.use(express.static(`${__dirname}/../`))
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
